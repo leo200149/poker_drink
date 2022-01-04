09a3d43b88b60a71dfdce4900d43cd8c13e20c6e
leo.chen <leo.chen@aceqilin.com>
2022-01-04 19:56:48 +0800

    update:調整字數

--- app.js
+++ app.js
@@ -26,15 +26,15 @@ function Card(num,color,name,desc){
 const card_define = {
 	0:new Card(0,"brown","喝酒遊戲","陪酒小姐喝起來！ 點擊即可開始遊戲"),
 	1:new Card(1,"orange","指定牌","可指定一個玩家喝掉1杯酒"),
-  2:new Card(2,"red","陪酒小姐","你必須扮演陪酒的角色，當場上有人被懲罰喝酒時，你也要跟著一起喝，直到下一位抽到「2」的人"),
+  2:new Card(2,"red","陪酒小姐","當場上有人喝酒就要跟著一起喝，直到下一位抽到「2」的人"),
   3:new Card(3,"orange","逛三園","果園、菜園、動物園，tempo遊戲，抓miss/重複的人喝1杯酒"),
-  4:new Card(4,"orange","照相機","可以在任意時候使用1次，當喊出照相機後，所有人都定格不能動，抓最慢的或第一個動的人喝1杯酒"),
-  5:new Card(5,"orange","摸鼻子","可以在任意時候使用1次，當摸鼻子後，所有人都要跟著摸鼻子，抓最慢的人喝1杯酒"),
+  4:new Card(4,"orange","照相機","可在任意時使用1次，當喊出照相機，所有人都不能動，抓最慢或第一個動的人喝1杯酒"),
+  5:new Card(5,"orange","摸鼻子","可在任意時使用1次，當摸鼻子，所有人都要跟著摸，抓最慢的人喝1杯酒"),
   6:new Card(6,"green","跳過","這回安心啦，沒有事情發生"),
   7:new Card(7,"orange","單挑牌","找一個人單挑任意遊戲，輸的人喝1杯酒"),
   8:new Card(8,"gold","尿尿牌","遊戲開始後，全部人就不能上廁所了，只有拿到尿尿牌的人可以去1次。"),
   9:new Card(9,"red","喝一杯","自己喝1杯酒"),
-  10:new Card(10,"gray","神經病","抽到的人，需宣告「我是神經病」，當宣告完成後，其他人不能跟神經病互動，抓第一個互動的人喝掉1杯酒"),
+  10:new Card(10,"gray","神經病","需宣告「我是神經病」完成後，其他人不能跟神經病互動，抓第一個互動的人喝掉1杯酒"),
   11:new Card(11,"red","上家喝","上家喝1杯酒"),
   12:new Card(12,"gold","女王牌","身為女王，可指定一個人幫你做1件事。"),
   13:new Card(13,"red","下家喝","下家喝1杯酒"),
@@ -63,12 +63,12 @@ function paintCard(card,point){
   ctx.fillText(card.num, CARD_WIDTH/2, CARD_HEIGHT/3);
   ctx.font = (50 *FONT_SIZE_RATE)+"px Arial";
   ctx.fillText(card.name, CARD_WIDTH/2, CARD_HEIGHT/3+(80 *FONT_SIZE_RATE));
- 	canvasTextAutoLine(card.desc,canvas, CARD_WIDTH/2, CARD_HEIGHT/3+(120 *FONT_SIZE_RATE),(30 *FONT_SIZE_RATE));
+ 	canvasTextAutoLine(card.desc,canvas, CARD_WIDTH/2, CARD_HEIGHT/3+(140 *FONT_SIZE_RATE),(30 *FONT_SIZE_RATE));
 }
 
 function canvasTextAutoLine(str,canvas,initX,initY,lineHeight){
     var ctx = canvas.getContext("2d"); 
- 		 ctx.font = (16 *FONT_SIZE_RATE)+"px Arial";
+ 		 ctx.font = (12 *FONT_SIZE_RATE)+"px Arial";
     var lineWidth = 0;
     var canvasWidth = CARD_WIDTH; 
     var lastSubStrIndex= 0; 
